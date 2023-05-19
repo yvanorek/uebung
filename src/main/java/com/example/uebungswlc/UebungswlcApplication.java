ddd77f8c4ae52511fd1e0d01e6c7ceb5230d49ee
VANOREK Yessica <yessica.vanorek@gmail.com>
2023-05-19 22:28:43 +0200

    erste version

---
+++ src/main/java/com/example/uebungswlc/UebungswlcApplication.java
@@ -0,0 +1,13 @@
+package com.example.uebungswlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebungswlcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UebungswlcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uebungswlc/UebungswlcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uebungswlc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebungswlcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
